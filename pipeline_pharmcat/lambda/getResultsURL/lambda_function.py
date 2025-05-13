a311d9c47c0d85d8ad0f9f29c087f30fbdf8c9a3
NickEdwards7502 <nickedwards7502@gmail.com>
2025-05-13 00:39:01 +0000

    MAINT: Remove indexed access from PGxFlow results

--- pipeline_pharmcat/lambda/getResultsURL/lambda_function.py
+++ pipeline_pharmcat/lambda/getResultsURL/lambda_function.py
@@ -8,19 +8,8 @@ from shared.dynamodb import check_user_in_project
 
 DPORTAL_BUCKET = os.environ["DPORTAL_BUCKET"]
 RESULT_SUFFIX = os.environ["RESULT_SUFFIX"]
-s3_resource = boto3.resource("s3")
+
 s3_client = boto3.client("s3")
-
-
-def read_from_s3(bucket_name, key, at, size):
-    byte_range = f"bytes={at}-{at + size - 1}"
-    response = s3_client.get_object(Bucket=bucket_name, Key=key, Range=byte_range)
-    return response["Body"].read()
-
-
-def read_size_from_s3(bucket_name, key):
-    response = s3_client.head_object(Bucket=bucket_name, Key=key)
-    return response["Body"].read()
 
 
 def lambda_handler(event, _):
@@ -36,11 +25,12 @@ def lambda_handler(event, _):
 
         check_user_in_project(sub, project_name)
 
-        # Handle small files (less than 5MB) by returning the entire content
-        file_size = s3_client.head_object(Bucket=DPORTAL_BUCKET, Key=results_path)[
-            "ContentLength"
-        ]
-        content = read_from_s3(DPORTAL_BUCKET, results_path, 0, file_size)
+        response = s3_client.get_object(
+            Bucket=DPORTAL_BUCKET,
+            Key=results_path,
+        )
+        content = response["Body"].read()
+
         return bundle_response(
             200,
             {
