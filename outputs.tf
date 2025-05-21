4374b031a25ca060270abf354f0eab900e002024
NickEdwards7502 <nickedwards7502@gmail.com>
2025-05-21 05:50:15 +0000

    DEV: Provide pgxflow backend bucket name and ARN to sBeacon module

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "api_url" {
   value       = "https://${aws_cloudfront_distribution.api_distribution.domain_name}/${aws_api_gateway_stage.PgxApi.stage_name}/"
   description = "URL used to invoke the API."
 }
+
+output "temp-bucket-name" {
+  value       = aws_s3_bucket.pgxflow-bucket.bucket
+  description = "Temporary bucket name"
+}
+
+output "temp-bucket-arn" {
+  value       = aws_s3_bucket.pgxflow-bucket.arn
+  description = "Temporary bucket ARN"
+}
