edf6d64e1e80d2fdb99775bacd5c9c2dd2242ebd
NickEdwards7502 <nickedwards7502@gmail.com>
2025-07-09 03:46:56 +0000

    FIX: Add cloudwatch eventbridge permissions for reference update triggers

--- pipeline_lookup/lambda.tf
+++ pipeline_lookup/lambda.tf
@@ -8,3 +8,14 @@ resource "aws_lambda_permission" "LambdaDbsnp" {
   principal     = "lambda.amazonaws.com"
   source_arn    = module.lambda-initLookup.lambda_function_arn
 }
+
+#
+# lookup-updateReferenceFiles Lambda Function
+#
+resource "aws_lambda_permission" "cloudwatch_lookup_reference_update_permission" {
+  statement_id  = "CloudwatchLookupReferenceUpdateAllowInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda-updateReferenceFiles.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.update_references_trigger.arn
+}
--- pipeline_pharmcat/lambda.tf
+++ pipeline_pharmcat/lambda.tf
@@ -30,3 +30,14 @@ resource "aws_lambda_permission" "LambdaPostprocessor" {
   principal     = "lambda.amazonaws.com"
   source_arn    = module.lambda-pharmcat.lambda_function_arn
 }
+
+#
+# pharmcat-updateReferenceFiles Lambda Function
+#
+resource "aws_lambda_permission" "cloudwatch_pharmcat_reference_update_permission" {
+  statement_id  = "CloudwatchPharmcatReferenceUpdateAllowInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda-updateReferenceFiles.lambda_function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.update_references_trigger.arn
+}
